6ac5a1a61429dfe8952dcf0de7a91c501245c912
Eduard <eduard.dev.de@gmail.com>
2024-10-29 07:33:35 +0100

    change read time to calculate function

--- functions/post-read-time.php
+++ functions/post-read-time.php
@@ -1,34 +1,16 @@
 <?php
-function add_read_time_meta_box()
-{
-  add_meta_box(
-    'read_time_meta_box',
-    'Read Time',
-    'show_read_time_meta_box',
-    'post',
-    'side',
-    'high'
-  );
-}
-add_action('add_meta_boxes', 'add_read_time_meta_box');
-
-function show_read_time_meta_box($post)
-{
-  $read_time = get_post_meta($post->ID, 'read_time', true);
-?>
-  <label for="read_time">Read Time (in minutes):</label>
-  <input type="number" id="read_time" name="read_time" value="<?php echo esc_attr($read_time); ?>" min="1">
-<?php
-}
 
 function save_read_time_meta($post_id)
 {
-  if (array_key_exists('read_time', $_POST)) {
-    update_post_meta(
-      $post_id,
-      'read_time',
-      sanitize_text_field($_POST['read_time'])
-    );
-  }
+  $post_content = get_post($post_id)->post_content;
+  $read_time = calculate_read_time($post_content);
+  update_post_meta($post_id, 'read_time', $read_time);
 }
 add_action('save_post', 'save_read_time_meta');
+
+function calculate_read_time($content)
+{
+  $word_count = str_word_count(strip_tags($content));
+  $read_time = ceil($word_count / 100);
+  return $read_time;
+}
